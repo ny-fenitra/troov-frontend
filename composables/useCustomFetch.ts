47c44ceb723aac502eb96d259077f7f6bda9b68d
Ny Fenitra <nyfenitra.rakoto@trente-douze.fr>
2024-06-17 18:17:00 +0300

    ✨ Intercept request to add token for authentifying request

--- composables/useCustomFetch.ts
+++ composables/useCustomFetch.ts
@@ -1,17 +1,35 @@
 import { toast } from 'vue3-toastify';
 
-const useCustomFetch = async (url: string, options: any) => {
+import useAuthStore from '~/stores/auth';
+
+const useCustomFetch = async (url: string, options?: any) => {
   const config = useRuntimeConfig();
+
+  const authStore = useAuthStore();
+  const { accessToken } = storeToRefs(authStore);
+  const { resetCredentials } = authStore;
 
   try {
     const data = await $fetch(url, {
       baseURL: config.public.baseURL,
       ...options,
+      headers: {
+        ...(accessToken.value
+          ? { Authorization: 'Bearer ' + accessToken.value }
+          : {}),
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
     });
 
     return { data, error: null };
   } catch (error: any) {
     error?.data?.msg && toast.error(error.data?.msg);
+
+    if (error.status === 401) {
+      resetCredentials();
+    }
+
     return { data: null, error };
   }
 };
